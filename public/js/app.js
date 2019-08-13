38dec49b80f4f9dd0aacabca4b92003c910136a1
DaCosta Smith <dacostasmith@macs-MacBook-Air.local>
2019-08-13 19:49:12 -0400

    show objects and create

--- public/js/app.js
+++ public/js/app.js
@@ -2,32 +2,32 @@
 const app = angular.module('RecipesApp', [])
 
 app.controller('MainController', ['$http', function ($http) {
-  // this.h2 = "Recipes!"
-  // this.createForm = {}
-  //
-  // this.createRecipe = () => {
-  //   $http({
-  //     method:'POST',
-  //     url: '/recipes',
-  //     data: this.createForm
-  //   }).then((response) => {
-  //     console.log(response.data);
-  //     this.recipes.unshift(response.data)
-  //     this.createForm = {}
-  //   }, (error) => {console.log(error)})
-  // }
-  //
-  //
-  // this.getRecipes = () => {
-  //   $http({
-  //     method: 'GET',
-  //     url: '/recipes'
-  //   }).then(response => {
-  //     this.recipes = response.data
-  //   }, error => {
-  //     console.log(error)
-  //   })
-  // }
-  //
-  // this.getRecipes()
+  this.h2 = "Recipes!"
+  this.createForm = {}
+
+  this.createRecipe = () => {
+    $http({
+      method:'POST',
+      url: '/recipes',
+      data: this.createForm
+    }).then((response) => {
+      console.log(response.data);
+      this.recipes.unshift(response.data)
+      this.createForm = {}
+    }, (error) => {console.log(error)})
+  }
+
+
+  this.getRecipes = () => {
+    $http({
+      method: 'GET',
+      url: '/recipes'
+    }).then(response => {
+      this.recipes = response.data
+    }, error => {
+      console.log(error)
+    })
+  }
+
+  this.getRecipes()
 }])
