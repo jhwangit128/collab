b968d4cea3ea96bd08bd189311f61146095999ef
DaCosta Smith <dacostasmith@macs-MacBook-Air.local>
2019-08-13 18:31:50 -0400

    removing node_module folder

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3001
 
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/meancrud'
+app.use(express.static('public'))
+// app.use(express.json())
+
+// mongoose.connect(mongoURI, { useNewUrlParser: true },
+//   () => console.log('MongoDB connection established:', mongoURI)
+// )
+// mongoose.connection()
 app.get('/', (req, res) => {
-  res.send('index file!! :)')
+  res.render('public/index.html')
 })
 
 app.listen(port, () => {
