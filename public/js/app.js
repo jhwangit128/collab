df712cd69ae45fd14c932e58095f7526a72c3b56
Jiha Hwang <jiha_hwang@jihas-mbp.lan>
2019-08-13 18:59:30 -0500

    delete added

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,17 @@
+// <<<<<<< HEAD
 const app = angular.module('RecipesApp', [])
 
 app.controller('MainController', ['$http', function ($http) {
+  this.h2 = "Recipes!"
+  this.createForm = {}
 
+  this.createRecipe = () => {
+    $http({
+      method:'POST',
+      url: '/recipes',
+      data: this.createForm
+    }).then((response) => {
+      console.log(response.data);
       this.recipes.unshift(response.data)
       this.createForm = {}
     }, (error) => {console.log(error)})
@@ -17,6 +27,16 @@ app.controller('MainController', ['$http', function ($http) {
     }, error => {
       console.log(error)
     })
+  }
+
+  this.deleteRecipe = (id) => {
+    $http({
+      method: 'DELETE',
+      url: '/recipes/' + id
+    }).then(response => {
+      const removeByIndex = this.recipes.findIndex(recipe => recipe._id === id)
+      this.recipes.splice(removeByIndex, 1)
+    }, error => console.log(error))
   }
 
   this.getRecipes()
