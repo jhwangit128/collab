29cc994ed100bb6a306719c5c4b0ad67dd53bfc3
Jiha Hwang <jiha_hwang@jihas-mbp.lan>
2019-08-13 18:50:12 -0500

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const app = angular.module('RecipesApp', [])
 
 app.controller('MainController', ['$http', function ($http) {
@@ -29,38 +28,4 @@ app.controller('MainController', ['$http', function ($http) {
   }
 
   this.getRecipes()
-=======
-// <<<<<<< HEAD
-const app = angular.module('RecipesApp', [])
-
-app.controller('MainController', ['$http', function ($http) {
-  // this.h2 = "Recipes!"
-  // this.createForm = {}
-  //
-  // this.createRecipe = () => {
-  //   $http({
-  //     method:'POST',
-  //     url: '/recipes',
-  //     data: this.createForm
-  //   }).then((response) => {
-  //     console.log(response.data);
-  //     this.recipes.unshift(response.data)
-  //     this.createForm = {}
-  //   }, (error) => {console.log(error)})
-  // }
-  //
-  //
-  // this.getRecipes = () => {
-  //   $http({
-  //     method: 'GET',
-  //     url: '/recipes'
-  //   }).then(response => {
-  //     this.recipes = response.data
-  //   }, error => {
-  //     console.log(error)
-  //   })
-  // }
-  //
-  // this.getRecipes()
->>>>>>> 0c3efe6211be12b7eb99de1e83e614d52be8818f
 }])
