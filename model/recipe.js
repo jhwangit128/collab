90d590bf808fd54551252ff94b4f046d572b1406
Jiha Hwang <jiha_hwang@jihas-mbp.lan>
2019-08-13 19:22:44 -0500

    recipe info added

--- model/recipe.js
+++ model/recipe.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const recipeSchema = mongoose.Schema({
   name: {type: String, required: true},
+  recipe: {type: String},
   ingredients: {type: String}
 })
 
